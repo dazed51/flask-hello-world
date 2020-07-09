46999b465b049f773b4b94529745876ac5bdeba7
dazed51 <dave.battaglia@gmail.com>
2020-07-09 14:03:44 -0400

    flask hello world - added dynmaic routes

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
 #decorator pattern
 @app.route("/")
 @app.route("/hello")
@@ -13,7 +16,20 @@ app = Flask(__name__)
 #define the view
 
 def hello_world():
-    return "hello, world"
+    return "hello, world!?!?!?!"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+#dynamic route with explict status codes
+@app.route("/name/<name>")
+def index(name):
+   if name.lower() == "mike" :
+       return "hello, {}".format(name)
+   else :
+       return "not found", 404
 
 #start the dev server using the run() method
 if __name__ == "__main__":
